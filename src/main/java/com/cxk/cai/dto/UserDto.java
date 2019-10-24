5f7e8694361a485db8927e6906632e5932c5a44d
lyf <isliyufeng@163.com>
2019-10-24 17:13:45 +0800

    李钰锋 加入了 登录页面

---
+++ src/main/java/com/cxk/cai/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.cxk.cai.dto;
+
+
+import lombok.Data;
+
+/**
+ * @author 喜闻乐见i
+ */
+@Data
+public class UserDto {
+    private String username;
+    private String password;
+}
--- src/main/java/com/cxk/cai/entity/User.java
+++ src/main/java/com/cxk/cai/entity/User.java
@@ -3,7 +3,6 @@ package com.cxk.cai.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
 /**
  * @author 喜闻乐见i
  */
